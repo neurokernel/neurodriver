a28a2de2b90c00c916ea98a48ac8586289b8afcd
Lev Givon <lev@columbia.edu>
2016-02-07 16:34:57 -0500

    Move split files into neurokernel/ dir, add license, authors, readme, and setup.py files.

---
+++ README.rst
@@ -0,0 +1,42 @@
+.. -*- rst -*-
+
+Neurodriver
+===========
+
+Package Description
+-------------------
+Neurodriver is a Python implementation of a Local Processing Unit (LPU) with support
+for various neuron and synapse models that can be used with the `Neurokernel
+<https://github.com/neurokernel/neurokernel>`_ framework.
+
+Prerequisites
+-------------
+Neurodriver requires Neurokernel and its various installation dependencies.
+
+Installation
+------------
+First, install Neurokernel as indicated in its `README
+<https://github.com/neurokernel/neurokernel/blob/master/README.rst>`_ file.
+
+Next, download the latest Neurodriver as follows: ::
+
+  git clone https://github.com/neurokernel/neurodriver.git
+
+Install the package as follows: ::
+
+  cd ~/neurodriver
+  python setup.py develop
+
+Authors & Acknowledgements
+--------------------------
+See the included `AUTHORS`_ file for more information.
+
+.. _AUTHORS: AUTHORS.rst
+
+License
+-------
+This software is licensed under the `BSD License
+<http://www.opensource.org/licenses/bsd-license.php>`_.
+See the included `LICENSE`_ file for more information.
+
+.. _LICENSE: LICENSE.rst
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys, os
+from glob import glob
+
+# Install setuptools if it isn't available:
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+from distutils.command.install_headers import install_headers
+from setuptools import find_packages
+from setuptools import setup
+
+NAME =               'neurodriver'
+VERSION =            '0.1'
+AUTHOR =             'Neurokernel Development Team'
+AUTHOR_EMAIL =       'neurokernel-dev@columbia.edu'
+URL =                'https://github.com/neurokernel/neurodriver/'
+MAINTAINER =         AUTHOR
+MAINTAINER_EMAIL =   AUTHOR_EMAIL
+DESCRIPTION =        'LPU class with support for various neuron and synapse models'
+LONG_DESCRIPTION =   DESCRIPTION
+DOWNLOAD_URL =       URL
+LICENSE =            'BSD'
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development']
+
+# Explicitly switch to parent directory of setup.py in case it
+# is run from elsewhere:
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+PACKAGES =           find_packages()
+
+if __name__ == "__main__":
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = AUTHOR,
+        author_email = AUTHOR_EMAIL,
+        license = LICENSE,
+        classifiers = CLASSIFIERS,
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        url = URL,
+        maintainer = MAINTAINER,
+        maintainer_email = MAINTAINER_EMAIL,
+        packages = PACKAGES,
+        include_package_data = True,
+        install_requires = [
+            'neurokernel >= 0.1',
+        ],
+        )
