f0db092c3f7cc50b1ecda4fb1f9b4375cf1abdc8
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2017-10-04 13:20:53 -0400

    add skeleton of LPU.graph.Graph

---
+++ neurokernel/LPU/graph.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+class Graph(object):
+    def __init__(self):
+        pass
+
+    def add_neuron(self):
+        """Wrapper of nx.graph.add_node
+        """
+        pass
+
+    def add_synape(self):
+        pass
+
+    def write_gexf(self, filename):
+        pass
+
+    def read_gexf(self, filename):
+        pass
