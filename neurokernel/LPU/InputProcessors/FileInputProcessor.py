61f3475599d56c7b00d0d06e3a8bc3f783b3aaf7
Tingkai Liu <tl2747@columbia.edu>
2018-01-10 13:37:49 -0500

    Update FileInputProcessor.py
    Move `self.pointer +=1` outside of the for loop for variables. Avoid incrementing pointer multiple times for multiple input variables over single timestep

--- neurokernel/LPU/InputProcessors/FileInputProcessor.py
+++ neurokernel/LPU/InputProcessors/FileInputProcessor.py
@@ -27,7 +27,7 @@ class FileInputProcessor(BaseInputProcessor):
         for var, dset in self.dsets.iteritems():
             if self.pointer+1 == dset.shape[0]: self.end_of_file=True
             self.variables[var]['input'] = dset[self.pointer,:]
-            self.pointer += 1
+        self.pointer += 1
         if self.end_of_file: self.h5file.close()
             
     def is_input_available(self):
