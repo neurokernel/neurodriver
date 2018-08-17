77c4b6320f9328343b3a4aa81ba43da1eb77b0b7
Yiyin Zhou <yiyin@ee.columbia.edu>
2018-08-17 00:16:59 -0400

    use dtype='S' when creating dataset for FileOutputProcessor to avoid problems with unicode

--- neurokernel/LPU/OutputProcessors/FileOutputProcessor.py
+++ neurokernel/LPU/OutputProcessors/FileOutputProcessor.py
@@ -6,7 +6,7 @@ class FileOutputProcessor(BaseOutputProcessor):
     def __init__(self, var_list, filename, sample_interval=1):
         self.fname = filename
         super(FileOutputProcessor, self).__init__(var_list, sample_interval)
-    
+
     def pre_run(self):
         self.h5file = h5py.File(self.fname, 'w')
         self.h5file.create_dataset('metadata',(),'i')
@@ -14,20 +14,19 @@ class FileOutputProcessor(BaseOutputProcessor):
         self.h5file['metadata'].attrs['sample_interval'] = self.sample_interval
         self.h5file['metadata'].attrs['dt'] = self.dt
         self.h5file['metadata'].attrs['DateCreated'] = datetime.now().isoformat()
-        
+
         for var,d in self.variables.items():
             self.h5file.create_dataset(var+'/data', (0,len(d['uids'])),\
                         d['output'].dtype, maxshape=(None,len(d['uids'])))
-            self.h5file.create_dataset(var+'/uids', data=np.array(d['uids']))
-            
+            self.h5file.create_dataset(var+'/uids',
+                                       data=np.array(d['uids'], dtype = 'S'))
+
     def process_output(self):
         for var, d in self.variables.items():
             self.h5file[var+'/data'].resize((self.h5file[var+'/data'].shape[0]+1,
                                              len(d['uids'])))
             self.h5file[var+'/data'][-1,:] = d['output'].reshape((1,-1))
-            
+
 
     def post_run(self):
         self.h5file.close()
-
-    
