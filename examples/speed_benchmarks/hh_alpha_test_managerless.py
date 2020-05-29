395c002b5dbc1eb56c9f331c5686ea94c8bb1cc0
Yiyin Zhou <yiyin@ee.columbia.edu>
2020-05-29 11:20:50 -0400

    added an example for managerless mode introduced in neurokernel commit 16f59da

--- examples/speed_benchmarks/hh_alpha_test_managerless.py
+++ examples/speed_benchmarks/hh_alpha_test_managerless.py
@@ -6,11 +6,10 @@ import itertools
 import dill
 import pickle
 import functools
+import uuid
 import networkx as nx
 import numpy as np
 from neurokernel.tools.logging import setup_logger
-import neurokernel.core_gpu as core
-from neurokernel.tools.misc import LPUExecutionError
 
 from neurokernel.LPU.LPU import LPU
 
@@ -18,9 +17,6 @@ from neurokernel.LPU.InputProcessors.FileInputProcessor import FileInputProcesso
 from neurokernel.LPU.InputProcessors.StepInputProcessor import StepInputProcessor
 from neurokernel.LPU.OutputProcessors.FileOutputProcessor import FileOutputProcessor
 from neurokernel.LPU.OutputProcessors.OutputRecorder import OutputRecorder
-
-import neurokernel.mpi_relaunch
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', default=False,
@@ -37,7 +33,7 @@ args = parser.parse_args()
 file_name = None
 screen = False
 if args.log.lower() in ['file', 'both']:
-    file_name = 'neurokernel.log'
+    file_name = 'neurokernel_{}.log'.format(str(uuid.uuid4())[:4])
 if args.log.lower() in ['screen', 'both']:
     screen = True
 logger = setup_logger(file_name=file_name, screen=screen)
@@ -60,6 +56,8 @@ def create_graph(N):
                     'E_K': -77.0,
                     'E_Na': 50.0,
                     'E_L': -54.387,
+                    'V': 0.0,
+                    'spike': 0,
                     'initV': np.random.rand()*70-70,
                     'initn': n,
                     'initm': np.random.rand(),
@@ -96,16 +94,15 @@ def create_graph(N):
 
             G.add_edge(id_i, synapse_id)
             G.add_edge(synapse_id, id_j)
+
     return G
 
 
-def simulation(dt, N, output_n, nsteps = 10000):
+def simulation(dt, N, output_n):
     start_time = time.time()
 
-    steps = nsteps
-    dur = nsteps * dt
-
-    man = core.Manager()
+    dur = 0.01
+    steps = int(np.round(dur/dt))
 
     G = create_graph(N)
     print("Creating graph completed in {} seconds.".format(time.time()-start_time))
@@ -114,29 +111,21 @@ def simulation(dt, N, output_n, nsteps = 10000):
     #comp_dict, conns = LPU.graph_to_dicts(G, remove_edge_id=False)
 
     fl_input_processor = StepInputProcessor('I', ['neuron_{}'.format(i) for i in range(N)], 20.0, 0.0, dur)
-    fl_output_processor = [FileOutputProcessor([('V', None), ('spike_state', None), ('g', None)],
-                                               'neurodriver_output_{}.h5'.format(output_n), sample_interval=1, cache_length=2000)]
-    #fl_output_processor = [] # temporarily suppress generating output
+    #fl_output_processor = [FileOutputProcessor([('V', None), ('g', ['synapse_neuron_{}_to_neuron_1'.format(i) for i in range(N)])],# ('spike_state', None), ('g', None), ('E', None)],
+    #                                           'neurodriver_output_{}.h5'.format(output_n), sample_interval=10, cache_length=2000)]
+    fl_output_processor = [] # temporarily suppress generating output
 
     #fl_output_processor = [OutputRecorder([('spike_state', None), ('V', None), ('g', None), ('E', None)], dur, dt, sample_interval = 1)]
 
-    man.add(LPU, 'ge', dt, 'pickle', pickle.dumps(G),
-            device=args.gpu_dev, input_processors=[fl_input_processor],
-            output_processors=fl_output_processor, debug=args.debug,
+    lpu = LPU(dt, 'obj', G,
+              device=args.gpu_dev, id = 'ge', input_processors=[fl_input_processor],
+              output_processors=fl_output_processor, debug=args.debug, manager = False,
             print_timing=False, time_sync=False,
             extra_comps=[])
-    print("Adding LPU completed in {} seconds.".format(time.time()-start_time))
-
+    print("Instantiating LPU completed in {} seconds.".format(time.time()-start_time))
     start_time = time.time()
-    man.spawn()
-    print('DEBUG: #simulation steps={}'.format(steps))
-    man.start(steps=steps)
-    print("Spawning LPUs Completed in {} seconds.".format(time.time()-start_time))
-    start_time = time.time()
-    try:
-        execution_time = man.wait(return_timing = True)
-    except LPUExecutionError
-        sys.exit(1)
+    # LPU.run includes pre_run, run_steps and post_run
+    lpu.run(steps = steps)
     compile_and_execute_time = time.time()-start_time
     print("LPUs Compilation and Execution Completed in {} seconds.".format(compile_and_execute_time))
     return compile_and_execute_time, execution_time
@@ -147,17 +136,8 @@ if __name__ == '__main__':
 
     #diff_dt = [5e-6, 1e-5, 5e-5, 1e-4, 1e-3]
     #diff_N = [2, 32, 128, 256, 512]
-
-    # comparison 1:
-    diff_dt = [1e-6] # run at the internal dt used by HH2.
-
-    # comparison 2:
-    #diff_dt = [1e-4] # Keep ddt at 1e-6 all the time.
-                     # The ODEs must be run at small dt,
-                     # but no need to store result for every 1e-6 second.
-                     # Also couple with sample_interval in FileOutputProcessor
-                     # to further reduce the storage.
-    diff_N = [2, 32, 128, 256, 512]
+    diff_dt = [1e-6] # change ddt accordingly
+    diff_N = [512]#[2, 32, 128, 256, 512]
     n_sim = 1
     i = 0
     for dt in diff_dt:
@@ -165,7 +145,7 @@ if __name__ == '__main__':
             sim_time.append([])
             compile_and_sim_time.append([])
             for t in range(n_sim + 1):
-                c, s = simulation(dt, N, i * n_sim + t, nsteps = 10000)
+                c, s = simulation(dt, N, i * n_sim + t)
                 sim_time[i].append(s)
                 compile_and_sim_time[i].append(c)
             sim_time[i].pop(0) # discard first result
