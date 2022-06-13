47f59f88e76dec141dd0e94b747998f4e8969201
Yiyin Zhou <yiyin@ee.columbia.edu>
2022-06-12 22:56:06 -0400

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 NAME =               'neurodriver'
-VERSION =            '0.3.0'
+VERSION =            '0.3.1'
 AUTHOR =             'Neurokernel Development Team'
 AUTHOR_EMAIL =       'neurokernel-dev@columbia.edu'
 URL =                'https://github.com/neurokernel/neurodriver/'
